199cdb8e00c40b300cc264218fa3c361698c0635
Leo en laptop <leonar2zb@gmail.com>
2025-01-10 21:51:41 -0300

    Creando mas tipos con zod y llamando asincrono

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,19 @@
 import { create } from "zustand";
 import axios from "axios";
+import { cryptoSchemas } from "./types";
+
 
 async function getCryptos() {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
     const { data: { Data } } = await axios(url)
-    console.log(Data)
+    const result = cryptoSchemas.safeParse(Data)
+    if (result.success)
+        return result.data
 }
 
 export const useCryptoStore = create(() => ({
-    fetchCryptos: () => {
-        getCryptos()
+    fetchCryptos: async () => {
+        const crytocurrencies = await getCryptos()
+        console.log(crytocurrencies)
     }
 }))
--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,15 @@ const Currency = z.object({
 })
 
 export type Currency = z.infer<typeof Currency>
+
+export const cryptoSchema = z.object({
+    CoinInfo: z.object({
+        FullName: z.string(),
+        Name: z.string()
+    })
+})
+
+export const cryptoSchemas = z.array(cryptoSchema)
+
+export type cryptoSchemaType = z.infer<typeof cryptoSchema>
+export type cryptoSchemasType = z.infer<typeof cryptoSchema>[]
